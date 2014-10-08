ccb784ae7e36eb909caf38baa51198f087d4f670
Bennett Siegel <bennett@Bennetts-MacBook-Pro.local>
2014-10-08 18:17:27 -0400

    initial commit

---
+++ src/p2p/p2p_networks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2014 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace nodetool
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xfc ,0x44, 0xda, 0x25 , 0x5d, 0xc1 , 0x61, 0x5c, 0xbb, 0xa7, 0x6c, 0x52, 0xc5, 0x68, 0xf7, 0x04} };
+}
